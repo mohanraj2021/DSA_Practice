3130fe0ee4216355370cba6ee4cd3f1429798466
mohanraj211 <mohanraj.n@hybridsolutions.com>
2026-02-25 10:28:59 +0530

    topKFrequent

--- random/main.go
+++ random/main.go
@@ -4,9 +4,14 @@ import "fmt"
 
 func groupAnagrams(strs []string) [][]string {
 	anagramMap := make(map[string][]string)
+	ch := make(chan string)
+	ch1 := make(chan string)
 	for _, str := range strs {
-		key := getFrequency(str)
-		anagramMap[key] = append(anagramMap[key], str)
+		go getFrequency(str, ch, ch1)
+	}
+	for i := 0; i < len(strs); i++ {
+		k := <-ch
+		anagramMap[k] = append(anagramMap[k], <-ch1)
 	}
 
 	var result [][]string
@@ -16,15 +21,40 @@ func groupAnagrams(strs []string) [][]string {
 	return result
 }
 
-func getFrequency(str string) string {
+func getFrequency(str string, ch, ch1 chan string) {
 	var freq = [26]int{}
 	for _, char := range str {
 		freq[char-'a']++
 	}
-	return fmt.Sprint(freq)
+	ch <- fmt.Sprint(freq)
+	ch1 <- str
 }
 
 func main() {
 	strs := []string{"eat", "tea", "tan", "ate", "nat", "bat"}
 	fmt.Println(groupAnagrams(strs))
+	fmt.Println(topKFrequent([]int{1, 1, 1, 2, 2, 4, 5, 4, 4, 6, 5, 5, 1, 3}, 3))
+}
+
+func topKFrequent(nums []int, k int) []int {
+
+	result := make([]int, 0)
+	var bucket = make([][]int, len(nums))
+	var mp = make(map[int]int)
+	for _, num := range nums {
+		mp[num]++
+	}
+	for k, v := range mp {
+		bucket[v] = append(bucket[v], k)
+	}
+
+	for i := len(bucket) - 1; i >= 0; i-- {
+		if len(bucket[i]) > 0 {
+			result = append(result, bucket[i]...)
+		}
+		if len(result) >= k {
+			break
+		}
+	}
+	return result
 }
