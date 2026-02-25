9b9d360b623fb6f4cb0adb901a1bf894c6a2c345
mohanraj211 <mohanraj.n@hybridsolutions.com>
2026-02-25 10:30:45 +0530

    topKFrequent

--- TwoPointers/main.go
+++ TwoPointers/main.go
@@ -1,12 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	findTargetArray := []int{2, 7, 11, 15}
 	findUniqeuArray := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
 	fmt.Println(findTarget(findTargetArray, 9))
 	fmt.Println(findUnique(findUniqeuArray))
+	fmt.Println(returnSorterSqaure([]int{-3, -1, 0, 1, 2}))
 }
 
 func findTarget(arr []int, target int) []int {
@@ -38,4 +42,18 @@ func findUnique(arr []int) []int {
 		}
 	}
 	return arr[:j]
+}
+
+func returnSorterSqaure(arr []int) []int {
+	j := 1
+	resArr := make([]int, len(arr))
+	for i := 1; i < len(arr); i++ {
+		x := arr[i] * arr[i]
+		y := arr[j-1] * arr[j-1]
+		if x != y {
+			resArr[i] = int(math.Max(float64(x), float64(y)))
+			j++
+		}
+	}
+	return resArr
 }
